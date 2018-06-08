a92658711d109eef76c893f3ccb6b0c34e551c5a
gongwenchao <wenchao.gong@windfindtech.com>
2018-06-08 16:16:30 +0800

    test---

--- TestApp/app/src/main/java/com/example/xajk/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/example/xajk/testapp/MainActivity.java
@@ -5,12 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-
-import com.google.gson.Gson;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
